2465a86d7181bdba7ca3486929ec84783414b26c
Jeremy S <jeresiv@gmail.com>
2014-08-11 17:14:30 -0400

    Added functionality for date interface

--- src/js/proactive-disclosure.js
+++ src/js/proactive-disclosure.js
@@ -13,16 +13,40 @@ $.ajax({
 		$("<div class='form-group'><div><input type='text' id='org-list-input' class='form-control search' placeholder='Filter organizations' /></div></div><ul class='list'></ul>").insertAfter("#organizationList summary");
 		
 		var options = {
-			item: '<li><div class="checkbox"><label><input class="id" type="checkbox" name="organizations" value="" /><span class="name"></span></label></div></li>',
-			valueNames: ['id' ,'name']
+			item: '<li><div class="checkbox"><label><input class="org-id" type="checkbox" name="organizations" value="" /><span class="org-label"></span></label></div></li>',
+			valueNames: ['org-id' ,'org-label']
 		};
 	
 		var organizationList = new List ('organizationList', options);
 		
 		organizationList.add(data);
-		organizationList.sort('name', { order: "asc" });
+		organizationList.sort('org-label', { order: "asc" });
 		
 		console.log("Added items to organization list");
+	}
+});
+
+// Simulate dynamic loading of fiscal-year list via API
+$.ajax({
+	url: "./data/yearList.json",
+	dataType: "json",
+	error: function (){
+		$("#fiscalYearList").append("<div class='alert alert-danger' role='alert'><span>Failed to load year list.</span></div>");
+	},
+	success: function (data){
+		$("#fiscalYearList").append("<div class='form-group'><div><input type='text' id='year-list-input' class='form-control search' placeholder='Filter fiscal years' /></div></div></div><ul class='list'></ul>");
+		
+		var options = {
+			item: '<li><div class="checkbox"><label><input class="year-num" type="checkbox" name="years" value="" /><span class="year-label"></span></label></div></li>',
+			valueNames: ['year-num' , 'year-label']
+		};
+	
+		var yearList = new List ('fiscalYearList', options);
+		
+		yearList.add(data);
+		yearList.sort('year-num', { order: "desc" });
+		
+		console.log("Added items to year list");
 	}
 });
 
@@ -46,7 +70,32 @@ $("#add-filters").bind("click", function () {
 // Date filter options
 $("input[name=date]").bind("click", function() {
 	var date = $("input[name=date]:checked").val();
+	var calendar = $("#calendarDates").detach();
+	var fiscalYear = $("#fiscalYearList").detach();
+	var quarter = $("#quarterList").detach();
 	
+	$("#advanced-date").empty();
+	
+	if (date === "calendar") {
+		$("#advanced-date").append(calendar);
+		$("#calendarDates").css({"display": "block"});
+		$("#advanced-date").append(fiscalYear);
+		$("#fiscalYearList").css({"display": "none"});
+		$("#advanced-date").append(quarter);
+		$("#quarterList").css({"display": "none"});
+	} else {				
+		$("#advanced-date").append(fiscalYear);
+		$("#fiscalYearList").css({"display": "block"});
+		if (date === "quarter") {
+			$("#advanced-date").append(quarter);
+			$("#quarterList").css({"display": "block"});
+		} else {
+			$("#advanced-date").append(quarter);
+			$("#quarterList").css({"display": "none"});
+		}
+		$("#advanced-date").append(calendar);
+		$("#calendarDates").css({"display": "none"});
+	}
 });
 
 // Apply button functionality
